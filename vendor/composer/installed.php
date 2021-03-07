ec378bbb81bc7d0f4df00f19db515c90b5d1e12c
Igor B <blinovigo@yandex.ru>
2021-03-07 10:44:46 +0300

    Установил twig и composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cbfe09306a12ec120ebba8f2cd5e643fce53c969',
+    'name' => 'vendor_name/geekbrainsphp2',
+  ),
+  'versions' => 
+  array (
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.44.2',
+      'version' => '1.44.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '138c493c5b8ee7cff3821f80b8896d371366b5fe',
+    ),
+    'vendor_name/geekbrainsphp2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cbfe09306a12ec120ebba8f2cd5e643fce53c969',
+    ),
+  ),
+);
